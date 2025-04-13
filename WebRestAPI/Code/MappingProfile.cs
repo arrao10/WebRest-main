72717159ea29cd0cc1480ef5daf4811114f37dfe
arrao10 <anirrao10@gmail.com>
2025-04-12 23:47:00 -0400

    first commit

---
+++ WebRestAPI/Code/MappingProfile.cs
@@ -0,0 +1,23 @@
+using System;
+using AutoMapper;
+using WebRest.EF.Models;
+
+namespace WebRestAPI.Code;
+
+ public class MappingProfile : Profile {
+     public MappingProfile() {
+         // Add as many of these lines as you need to map your objects
+         CreateMap<Customer, Customer>();
+         CreateMap<Address, Address>();
+         CreateMap<AddressType, AddressType>();
+         CreateMap<CustomerAddress, CustomerAddress>();
+         CreateMap<Gender, Gender>();
+         CreateMap<Order, Order>();
+         CreateMap<OrdersLine, OrdersLine>();
+         CreateMap<OrderState, OrderState>();
+         CreateMap<OrderStatus, OrderStatus>();
+         CreateMap<Product, Product>();
+         CreateMap<ProductPrice, ProductPrice>();
+         CreateMap<ProductStatus, ProductStatus>();
+     }
+ }
---
+++ WebRestAPI/Program.cs
@@ -0,0 +1,53 @@
+using System.Text.Json.Serialization;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+// DO NOT FORGET TO UNCOMMENT THIS LINE
+using WebRest.EF.Data;
+using WebRestAPI.Code;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("WebRestOracleConnection");
+
+// DO NOT FORGET TO UNCOMMENT THIS LINE
+
+builder.Services.AddDbContext<WebRestOracleContext>
+    (options => options.UseOracle(connectionString)
+    .UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()))
+    );
+
+
+// Auto Mapper Configurations
+var mapperConfig = new MapperConfiguration(mc =>
+{
+    mc.AddProfile(new MappingProfile());
+}); 
+
+IMapper mapper = mapperConfig.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
+builder.Services.AddControllers().AddJsonOptions(x =>
+   x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve);
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
